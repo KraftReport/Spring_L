09d1d835cc8060614f1968e657477ae80ec7e963
Myo Set Paing <kraftreport04@gmail.com>
2025-02-07 22:25:19 +0630

    leetcode

--- zymal/src/main/java/com/kraft/zymal/ZymalApplication.java
+++ zymal/src/main/java/com/kraft/zymal/ZymalApplication.java
@@ -37,6 +37,37 @@ public class ZymalApplication {
 		return result.get(rowIndex);
  
 	}
+	
+    public static String mergeAlternately(String word1, String word2) {
+    	var result = "";
+        var firstWordSize = word1.length();
+        var secondWordSize = word2.length();
+        
+        var commonLength = firstWordSize >= secondWordSize ? firstWordSize : secondWordSize;
+        
+        for(int i = 0 ; i < commonLength ; i++) {
+        	
+        	var boxOne = "";
+        	var boxTwo = "";
+        	
+        	try {
+        		boxOne = ""+word1.charAt(i);
+        	}catch(Exception ex) {
+        		boxOne = "";
+        	}
+        	
+        	try {
+        		boxTwo = ""+word2.charAt(i);
+        	}catch(Exception ex) {
+        		boxTwo = "";
+        	} 
+        	
+        	result += boxOne+boxTwo;
+        }
+        
+        return result;
+        
+    }
 
 	public static void main(String[] args) {
 //		SpringApplication.run(ZymalApplication.class, args);
@@ -47,6 +78,8 @@ public class ZymalApplication {
 			System.out.println(lis);
 			
 		}
+		
+		System.out.println(mergeAlternately("hlo34","el3434"));
 	}
 
 }
