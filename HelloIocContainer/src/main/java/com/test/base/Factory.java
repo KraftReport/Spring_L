e1b2447158a2cd126cdde43453d7cdc2845cdf5c
Myo Set Paing <kraftreport04@gmail.com>
2025-03-08 23:13:13 +0630

    learn spring

---
+++ HelloIocContainer/src/main/java/com/test/base/Factory.java
@@ -0,0 +1,8 @@
+package com.test.base;
+
+public class Factory {
+
+	public static HelloBean build() {
+		return new HelloBean();
+	}
+}
--- HelloIocContainer/src/test/java/com/test/base/HelloTest.java
+++ HelloIocContainer/src/test/java/com/test/base/HelloTest.java
@@ -1,18 +1,32 @@
 package com.test.base;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.test.AppConfig;
 
 public class HelloTest {
 
 	@Test
 	void testOne() {
-		try (var context = new GenericXmlApplicationContext()) {
-			context.load("classpath:bean.xml");
-			context.refresh(); 
+		try (var context = new AnnotationConfigApplicationContext(AppConfig.class)) { 
+			var dateBean = context.getBean(Date.class);
+			System.out.println(dateBean.toString());
 			var helloBean = context.getBean(HelloBean.class);
 			helloBean.sayHello();
+		}
+	}
+	
+	@Test
+	void testTwo() {
+		try(var context = new GenericXmlApplicationContext()){
+			context.load("classpath:bean.xml");
+			context.refresh();
+			var bean = context.getBean(HelloBean.class);
+			bean.sayHello();
 		}
 	}
 
