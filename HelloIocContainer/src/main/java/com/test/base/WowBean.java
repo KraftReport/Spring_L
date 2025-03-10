d18de4ef2abcbff885f40b159b89d27a0529e42e
Myo Set Paing <kraftreport04@gmail.com>
2025-03-10 23:57:31 +0630

    ioc

--- HelloIocContainer/src/main/java/com/test/base/WowBean.java
+++ HelloIocContainer/src/main/java/com/test/base/WowBean.java
@@ -1,8 +1,13 @@
 package com.test.base;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
 import com.test.util.MyBean;
 
 @MyBean("haha")
+@Scope("singleton")
+@Lazy
 public class WowBean {
 
 	public void wow() {
